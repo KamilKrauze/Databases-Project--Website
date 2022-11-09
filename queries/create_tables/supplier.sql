ceee7381aecb0c7213c6cc4b268b324436bf3172
Kamil Krauze <kamila.p.krauze@gmail.com>
2022-11-09 19:03:15 +0000

    Added basic constraint and default for "title" col

--- queries/create_tables/supplier.sql
+++ queries/create_tables/supplier.sql
@@ -1,14 +1,16 @@
 CREATE TABLE Supplier (
-	supplierID int NOT NULL,
+	supplierID int NOT NULL AUTO_INCREMENT,
 	supplierName varchar(255) NOT NULL,
 	addr varchar(255) NOT NULL,
 	postcode varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	phoneNo varchar(11) NOT NULL,
-	contactTitle varchar(255) NOT NULL,
+	contactTitle varchar(255) NOT NULL DEFAULT "Mr",
 	contactFirstName varchar(255) NOT NULL,
 	contactLastName varchar(255) NOT NULL,
 	
 	PRIMARY KEY (supplierID),
-	UNIQUE (supplierID)
+	UNIQUE (supplierID),
+
+	CHECK (contactTitle="Mr" OR contactTitle="Mrs" OR contactTitle="Ms" OR contactTitle="Dr")
 );
