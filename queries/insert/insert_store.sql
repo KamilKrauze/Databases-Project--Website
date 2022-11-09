3f8a852de3d348a1a68e58b243370ec782879e5a
Kamil Krauze <kamila.p.krauze@gmail.com>
2022-11-09 19:10:45 +0000

    SQL -> insert_store

---
+++ queries/insert/insert_store.sql
@@ -0,0 +1,8 @@
+INSERT INTO Store (addr, postCode, phoneNo, branchName)
+VALUES
+	('47 Johnston Gardens West, Peterculter, Aberdeen', 'AB14 0LB', '01224733926', 'The Vinyl Countdown'),
+	('319 Perth Road', 'DD2 1NN', '07348738278', 'Tunesday'),
+	('11 Market Pl, Saint Ives', 'TR26 1RZ', '07478538798', 'Discworld'),
+	('274 Perth Road', 'DD2 1AE', '09845798479', 'All Night Song'),
+	('6 Birch Way, Osprey Village, Inverurie', 'AB51 6AP', '02479858984', 'Vinyl Fantasy'),
+	('7 Harrison Square, Esplanade, Lerwick, Shetland', 'ZE1 0LL', '01749872398', 'Cassette-me-not');
